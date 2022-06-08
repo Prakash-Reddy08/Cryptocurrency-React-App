bdc2d3c6d78a4692ee169734c1ee0fd543ea27dd
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-06-08 20:40:21 +0530

    add code splitting

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home';
-import Cryptos from './Cryptos';
-import News from './News';
-import SingleCrypto from './SingleCrypto';
-import Error from './Error';
-
-export { Home, Cryptos, News, SingleCrypto, Error };
